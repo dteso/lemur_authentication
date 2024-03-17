11202beb20aef69bb1ab0657ffae1ee223d41472
dteso <dtesodev@gmail.com>
2024-03-17 20:40:05 +0100

    adding new permissions

--- seeder.ts
+++ seeder.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
 import Server from './servers/server';
 import PermissionService from './services/permission.service';
-import { executePgQuery } from './database/execute-pg-query';
-
-dotenv.config();
+import RoleService from './services/role.service';
 
 console.log("[INFO] STARTING NODE-TS SERVER");
 
@@ -14,11 +11,20 @@ const permissions = [
     },
     {
         name: 'USER_VIEW',
-        description: 'Allows to create view users.'
+        description: 'Allows to view users.'
     },
+    {
+        name: 'USER_VIEW_SELF',
+        description: 'Allows to view own user.'
+    },
+
     {
         name: 'USER_DELETE',
         description: 'Allows to delete users.'
+    },
+    {
+        name: 'USER_DELETE_SELF',
+        description: 'Allows to delete own user.'
     },
     {
         name: 'USER_LIST',
@@ -27,6 +33,10 @@ const permissions = [
     {
         name: 'USER_EDITION',
         description: 'Allows to modify users.'
+    },
+    {
+        name: 'USER_EDITION_SELF',
+        description: 'Allows to edit own user.'
     },
     {
         name: 'FILE_CREATE',
@@ -49,86 +59,91 @@ const permissions = [
         description: 'Allows to edit files.'
     },
     {
-        name: 'FOLDER_CREATE'
+        name: 'FOLDER_CREATE',
+        description: 'Allows to create folders in server directory.'
     },
     {
-        name: 'FOLDER_VIEW'
+        name: 'FOLDER_VIEW',
+        description: 'Allows to view folder in server directory.'
     },
     {
-        name: 'FOLDER_DELETE'
+        name: 'FOLDER_DELETE',
+        description: 'Allows to delete any folder in server directory.'
     },
     {
-        name: 'FOLDER_LIST'
+        name: 'FOLDER_LIST',
+        description: 'Allows to view all folders info in server directory.'
     },
     {
-        name: 'FOLDER_EDITION'
+        name: 'FOLDER_EDITION',
+        description: 'Allows to edit folders info in server directory.'
     },
     {
-        name: 'S3_CREATE'
+        name: 'S3_CREATE',
+        description: 'Allows to upload s3 objects to bucket.'
     },
     {
-        name: 'S3_VIEW'
+        name: 'S3_VIEW',
+        description: 'Allows to view s3 objects from bucket.'
     },
     {
-        name: 'S3_DELETE'
+        name: 'S3_DELETE',
+        description: 'Allows to delete s3 objects from bucket.'
     },
     {
-        name: 'S3_LIST'
+        name: 'S3_LIST',
+        description: 'Allows to lista all s3 objects in bucket.'
     },
     {
-        name: 'S3_EDITION'
+        name: 'S3_EDITION',
+        description: 'Allows to edit all s3 objects in bucket.'
     },
     {
-        name: 'ROLE_CREATE'
+        name: 'ROLE_CREATE',
+        description: 'Allows to create new roles in database.'
     },
     {
-        name: 'ROLE_VIEW'
+        name: 'ROLE_VIEW',
+        description: 'Allows to view roles in database.'
     },
     {
-        name: 'ROLE_DELETE'
+        name: 'ROLE_DELETE',
+        description: 'Allows to deñete new roles from database.'
     },
     {
-        name: 'ROLE_LIST'
+        name: 'ROLE_LIST',
+        description: 'Allows to list roles in database.'
     },
     {
-        name: 'ROLE_EDITION'
+        name: 'ROLE_EDITION',
+        description: 'Allows to edit roles in database.'
     },
     {
-        name: 'PERMISSION_CREATE'
+        name: 'PERMISSION_CREATE',
+        description: 'Allows to create new permisisons in database.'
     },
     {
-        name: 'PERMISSION_VIEW'
+        name: 'PERMISSION_VIEW',
+        description: 'Allows to view permissions in database.'
     },
     {
-        name: 'PERMISSION_DELETE'
+        name: 'PERMISSION_DELETE',
+        description: 'Allows to delete permissions from database.'
     },
     {
-        name: 'PERMISSION_LIST'
+        name: 'PERMISSION_LIST',
+        description: 'Allows to list permissions in database.'
     },
     {
-        name: 'PERMISSION_EDITION'
+        name: 'PERMISSION_EDITION',
+        description: 'Allows to edit permissions in database.'
     }
 ]
 
-const scripts = [
-    "database/create-permissions.sql",
-    "database/create-roles.sql",
-    "database/create-users.sql",
-    // "database/create-role-permissions.sql",
-];
-
 async function seedDatabase() {
-    for (const script of scripts) {
-        try {
-            await executePgQuery(script);
-        } catch (error) {
-            console.error('Error al ejecutar el script:', error);
-            // Si ocurrió un error, puedes decidir si quieres continuar con el siguiente script o detener la ejecución
-            // Por ejemplo, puedes lanzar una excepción para detener la ejecución o simplemente continuar con el siguiente script
-        }
-    }
 
     const permissionService = new PermissionService();
+    const roleService = new RoleService();
 
     console.log(" SEEDING PERMISSIONS ... ");
 
@@ -136,6 +151,19 @@ async function seedDatabase() {
         console.log("> inserting: ", permission);
         permissionService.createPermission(permission);
     }
+
+    let role = await roleService.createRole({
+        name: 'ADMIN_ROLE',
+        description: 'The super user for the application.'
+    });
+
+    const permissionIds = (await permissionService.getAllPermissions()).map((permission: any) => permission.id);
+    role = await roleService.assignPermissionsToRoleUsingId(role.id, permissionIds);
+
+    if (role) {
+        console.info("[DATABASE INITILIZATION SUCCESS] Remember to /registrer a new USER and assign ADMIN_ROLE (roleId: '1') to start managing users.")
+    }
+
 }
 
 const server = new Server();
